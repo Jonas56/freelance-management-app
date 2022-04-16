86b4ce4044f45139a6ebb4bcf57f755ec544d68f
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-04-16 23:42:09 +0000

    Add HibernateUtil

---
+++ src/main/java/com/config/HibernateUtil.java
@@ -0,0 +1,41 @@
+package com.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import net.bytebuddy.asm.Advice.This;
+
+public class HibernateUtil {
+    private static StandardServiceRegistry standardServiceRegistry;
+    private static SessionFactory sessionFactory;
+
+    static{
+        if (sessionFactory == null) {
+            try {
+                // Create StandardServiceRegistry
+                standardServiceRegistry = new StandardServiceRegistryBuilder()
+                        .configure()
+                        .build();
+                // Create MetadataSources
+                MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
+                // Create Metadata
+                Metadata metadata = metadataSources.getMetadataBuilder().build();
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (standardServiceRegistry != null) {
+                    StandardServiceRegistryBuilder.destroy(standardServiceRegistry);
+                }
+            }
+        }
+    }
+    //Utility method to return SessionFactory
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    
+}
