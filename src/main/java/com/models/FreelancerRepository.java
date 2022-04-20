faf0d0318dadaad93000d675e17b5104ee2aaed1
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-04-20 01:09:09 +0000

    Add CRUD Freelancer

--- src/main/java/com/models/FreelancerRepository.java
+++ src/main/java/com/models/FreelancerRepository.java
@@ -1,6 +1,9 @@
 package com.models;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.PersistenceException;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -19,7 +22,7 @@ public class FreelancerRepository {
     	Transaction tx = session.beginTransaction();
     	try{
             session.saveOrUpdate(freelancer);
-            session.flush() ;
+            session.flush();
             tx.commit();
             return true;
         } catch (HibernateException e) {
@@ -32,18 +35,58 @@ public class FreelancerRepository {
     }
 
     public boolean deleteFreelancer(String username){
-		return false;
+    	Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		try {
+			// TODO Handle Exception ???
+			Freelancer freelancer = new Freelancer();
+			freelancer.setUsername(username);
+			session.delete(freelancer);
+			session.flush();
+			tx.commit();
+			return true;
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+			tx.rollback();
+			return false;
+		} finally {
+			session.close();
+		}
     }
 
-    public Freelancer getFreelancer(String username) {
+    public Freelancer getFreelancer(int id) {
+    	Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		try {
+			Freelancer freelancer = session.get(Freelancer.class, id);
+			session.flush();
+			tx.commit();
+			return freelancer;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			session.close();
+		}
 		return null;
     }
 
     public List<Freelancer> getAllFreelancers() {
+    	Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		try {
+			List<Freelancer> freelancers = new ArrayList<>();
+			freelancers = session.createQuery("from freelancer").list();
+			session.flush();
+			tx.commit();
+			return freelancers;
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			session.close();
+		}
 		return null;
     }
 
-    public int countFreelancers(){
-		return 0;
-    }
 }
