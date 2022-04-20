e9a256a93466616196a40892d1329a785325da5b
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-04-20 01:08:29 +0000

    Update Hibernate config

--- src/main/java/com/config/HibernateUtil.java
+++ src/main/java/com/config/HibernateUtil.java
@@ -6,8 +6,6 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import net.bytebuddy.asm.Advice.This;
-
 public class HibernateUtil {
     private static StandardServiceRegistry standardServiceRegistry;
     private static SessionFactory sessionFactory;
