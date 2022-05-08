375d084862c5ac735fcc93b2a2832ac3f642c29f
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-05-08 10:04:45 +0000

    Add validation helper function.

--- src/main/java/com/freelance/utils/AnnonceHelper.java
+++ src/main/java/com/freelance/utils/AnnonceHelper.java
@@ -1,12 +1,10 @@
 package com.freelance.utils;
 
 import com.freelance.beans.Annonce;
-import com.freelance.beans.AnnonceImage;
 import com.freelance.models.IAnnonceRepository;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class AnnonceHelper {
 
@@ -16,7 +14,7 @@ public class AnnonceHelper {
         this.annonceRepository = annonceRepository;
     }
 
-    public String findByIdAndDelete(String id){
+    public String findByIdAndDelete(String id) {
         String errorMessage = "Unable to delete annonce!";
         Integer annonceId = Integer.valueOf(id);
         Annonce annonce = annonceRepository.findById(annonceId);
@@ -26,27 +24,22 @@ public class AnnonceHelper {
                 errorMessage = "Annonce Deleted Successfully!";
             }
         }
-        return  errorMessage;
+        return errorMessage;
     }
 
-    public Annonce getAnnonceFromUserInput(HttpServletRequest req){
+    public Annonce getAnnonceFromUserInput(HttpServletRequest req) {
         Annonce annonce = new Annonce();
         Integer annonceId = null;
-        try{
-                annonceId = Integer.parseInt(req.getParameter("annonceId"));
-        }catch (Exception ignored){
+        try {
+            annonceId = Integer.parseInt(req.getParameter("annonceId"));
+        } catch (Exception ignored) {
         }
         String titre = req.getParameter("titre");
         String description = req.getParameter("description");
-        List<AnnonceImage> imageList = new LinkedList<>();
-        // FIXME: Get Annonce images as List
-        //   List<String> requestImageList =
-        //   Collections.singletonList(req.getParameter("images"));
-        //   for (String image : requestImageList) {
-        //      imageList.add(new AnnonceImage(image));
-        //   }
-        imageList.add(new AnnonceImage(req.getParameter("image")));
-        if(annonceId != null){
+        LinkedList<String> imageList = new LinkedList<>();
+        imageList.add(req.getParameter("image"));
+        // VALIDATION!
+        if (annonceId != null) {
             annonce.setId(annonceId);
         }
         annonce.setAnnonceImages(imageList);
@@ -54,5 +47,31 @@ public class AnnonceHelper {
         annonce.setTitre(titre);
 
         return annonce;
+    }
+
+    public String validateUserInput(Annonce annonce) {
+        String titre = annonce.getTitre();
+        String description = annonce.getDescription();
+        LinkedList<String> imageList = annonce.getAnnonceImages();
+        String message = null;
+        if (imageList.get(0) == null || titre == null || description == null) {
+            message = "Input fields cannot be null!";
+        } else if (titre.isEmpty() || description.isEmpty() || imageList.get(0).isEmpty()) {
+            message = "Input fields cannot be empty!";
+        }
+        return message;
+    }
+
+    public String findByIdAndUpdate(String id, Annonce annonceUpdated){
+        String errorMessage = "Unable to update annonce!";
+        Integer annonceId = Integer.valueOf(id);
+        Annonce annonceExists = annonceRepository.findById(annonceId);
+        if (annonceExists != null) {
+            boolean annonceDeleted = annonceRepository.update(annonceUpdated);
+            if (annonceDeleted) {
+                errorMessage = "Annonce updated Successfully!";
+            }
+        }
+        return errorMessage;
     }
 }
