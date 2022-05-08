e2abf975ccd1c5cc44bdc0d4c8bc7f3584f31a58
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-05-08 10:02:17 +0000

    Update Annonce bean. Remove relationship

--- src/main/java/com/freelance/beans/Annonce.java
+++ src/main/java/com/freelance/beans/Annonce.java
@@ -13,36 +13,33 @@ public class Annonce implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    @NotEmpty
+    private Integer id;
+    @Column(nullable = false)
     private String titre;
-    @NotEmpty
+    @Column(nullable = false)
     private String description;
 
-    @OneToMany(mappedBy = "annonce",
-            fetch = FetchType.EAGER,
-            cascade = CascadeType.ALL)
-    private List<AnnonceImage> annonceImages;
-
+    @Column(name = "images", nullable = false)
+    private LinkedList<String> annonceImages;
 
     @ManyToOne
-    @JoinColumn(name = "freelancer_id", referencedColumnName = "id")
+    @JoinColumn(name = "freelancer_id")
     private Freelancer freelancer;
 
     public Annonce() {
     }
 
-    public Annonce(String titre, String description, List<AnnonceImage> annonceImages) {
+    public Annonce(String titre, String description, LinkedList<String> annonceImages) {
         this.titre = titre;
         this.description = description;
         this.annonceImages = annonceImages;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -62,11 +59,11 @@ public class Annonce implements Serializable {
         this.description = description;
     }
 
-    public List<AnnonceImage> getAnnonceImages() {
+    public LinkedList<String> getAnnonceImages() {
         return annonceImages;
     }
 
-    public void setAnnonceImages(List<AnnonceImage> annonceImages) {
+    public void setAnnonceImages(LinkedList<String> annonceImages) {
         this.annonceImages = annonceImages;
     }
 
